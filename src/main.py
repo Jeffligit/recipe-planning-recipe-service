340e14cc0cee34d46200b65ee5ab3409d322062c
Jeff Li <54647089+Jeffligit@users.noreply.github.com>
2024-06-02 16:40:31 -0400

    KAN-9: Made connection to database (#2)
    * Made connection to database
    
    * Add launch instructions to readme
    
    ---------
    
    Co-authored-by: Jeff Li <jeffli@Jeffs-MacBook-Pro.local>

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+dynamodb = boto3.resource(
+    "dynamodb", 
+    region_name=os.getenv('DB_REGION_NAME'), 
+    aws_access_key_id=os.getenv('DB_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('DB_SECRET_ACCESS_KEY')
+    ) 
 
 app = FastAPI()
 
 @app.get('/')
 def root():
-    return {"message" : "Hello World"}
+    table = dynamodb.Table('Recipe')
+    # response = table.get_item(
+    #     Key = {"name": "Ramen"}
+    # )
+
+    # return response["Item"]
