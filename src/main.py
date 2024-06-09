e76954c944294517593811f3d4cb157d6e971a12
Jeff Li <jeffli@Jeffs-MacBook-Pro.local>
2024-06-08 22:27:42 -0400

    Added docstrings to the functions in main and moved around imports to alphabetical order

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
+from typing import Annotated
+
+from .auth.user_auth import create_access_token, decode_access_token, verify_password
+from .database import SessionLocal, engine
+from .models import Base
+from .schemas import User, UserCreate, TokenData, Token
+from .user.crud import create_user, get_user_by_username, get_user_by_email, get_user
+
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError 
 from sqlalchemy.orm import Session
-from . import models
-from .database import SessionLocal, engine
-from typing import Annotated
-from .schemas import User, UserCreate, TokenData, Token
-from .auth.user_auth import create_access_token, decode_access_token, verify_password
-from .user.crud import create_user, get_user_by_username, get_user_by_email, get_user
- 
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -28,9 +29,20 @@ def root():
 
 @app.get('/health')
 def health():
+    '''
+    Checks if the service is healthy
+    '''
     return ""
 
-def verify_jwt(token: Annotated[str, Depends(oauth2_scheme)]):
+def verify_jwt(token: Annotated[str, Depends(oauth2_scheme)]) -> TokenData:
+    '''
+    Verifies the JWT (JSON Web Token)
+
+    Paramater: 
+        token: a JWT token supplied in the header of the request
+
+    Return: TokenData object
+    '''
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -56,6 +68,15 @@ def verify_jwt(token: Annotated[str, Depends(oauth2_scheme)]):
     
 @app.get('/user/', response_model=User)
 def read_user(token_data: Annotated[TokenData, Depends(verify_jwt)], db: Session = Depends(get_db)):
+    '''
+    Retrieves user using information provided in the JWT token
+
+    Parameters: 
+        token_data: TokenData object created from verifying the jwt token. Has a dependency function verify_wt
+        db: database session
+
+    Returns: response_model=User json of User schema
+    '''
     user = get_user(db, user_id=token_data.user_id)
     if user is None:
         raise HTTPException(
@@ -68,7 +89,15 @@ def read_user(token_data: Annotated[TokenData, Depends(verify_jwt)], db: Session
 
 @app.post('/token')
 def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)) -> Token:
-    # log in with email
+    '''
+    Verifies login credentials from from received in the header. Creates a jwt token from the information provided
+
+    Parameter:
+        form_data: login credentials provided in the header.
+        db: db session
+    Returns: 
+        Token: jwt token object
+    '''
     user = get_user_by_email(db, form_data.username)
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
@@ -83,7 +112,15 @@ def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depen
 
 @app.post('/signup', response_model=User)
 def signup(user: UserCreate, db: Session = Depends(get_db)):
-    # check if email is in use
+    '''
+    Creates a user in the database
+
+     Parameters: 
+        user: UserCreate object in the response body
+        db: database session
+
+    Returns: response_model=User json of User schema
+    '''
     existing_user = get_user_by_email(db, user.email)
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
