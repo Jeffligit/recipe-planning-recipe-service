adeb9c2f66ff9d03551891df8857ae69dd4b2eb3
laeonej <laeonej@seas.upenn.edu>
2024-07-10 18:35:19 -0400

    implemented paginating recipe results

--- src/test/test_recipe.py
+++ src/test/test_recipe.py
@@ -1,4 +1,4 @@
-from src.recipe.crud import create_recipe, get_recipe
+from src.recipe.crud import create_recipe, get_recipe, get_paginated_recipes
 from src.models import Recipe
 from src.schemas import RecipeCreate
 
@@ -18,3 +18,21 @@ class TestRecipe:
     def test_get_recipe(self, session):
         expected = get_recipe(session, 1)
         assert expected.title == "test recipe"
+
+    def test_get_paginated_recipes(self, session):
+        for i in range(10):
+            test_recipe = RecipeCreate(
+                title="test recipe number " + str(i + 2),
+                description="some steps",
+                prep_time=1,
+                cook_time=1
+            )
+            create_recipe(session, test_recipe, user_id=i)
+
+        #getting first 5 recipes
+        recipe_list = get_paginated_recipes(session, 1, 5)
+
+        for recipe in recipe_list:
+            print(f"{recipe.id}, {recipe.title}")
+        
+        assert len(recipe_list) == 5
